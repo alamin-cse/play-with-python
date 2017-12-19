e85de4e4f6fb4953b79061aac9bcd583d359ff58
Al Amin <aminal.cse@gmail.com>
2017-12-20 00:46:31 +0100

    The final version of the vowels program, “vowels7.py", took advantage of Python’s set data structure to considerably shrink the list-based “vowels3.py” code, while still providing the same functionality
    The final version of the vowels
    program, “vowels7.py", took
    advantage of Python’s set data
    structure to considerably shrink
    the list-based “vowels3.py” code,
    while still providing the same
    functionality

--- tasks/vowels7.py
+++ tasks/vowels7.py
@@ -4,3 +4,12 @@ found = vowels.intersection(set(word))
 
 for vowel in found:
     print(vowel)
+
+
+"""The final version of the vowels
+program, “vowels7.py", took
+advantage of Python’s set data
+structure to considerably shrink
+the list-based “vowels3.py” code,
+while still providing the same
+functionality"""
